62a6e5d081dab49c10095d252dd7ea798e829e67
Parag Parate <123316083+paragoo7@users.noreply.github.com>
2023-07-22 16:51:46 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (7.56%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,14 +1,13 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
         int n = mat.length;
-        int sum =0;
-        for(int i =0; i<n; i++){
-            for(int j=0 ; j<n; j++ ){
-                if(i==j || i+j == n-1){
-                 sum = sum+ mat[i][j];
-                }
-            }
+        int localSum = 0;
+        for (int i = 0; i < n; i++) {
+            localSum += mat[i][i];
         }
-        return sum;
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            localSum += mat[i][j];
+        }
+        return (n % 2 == 0) ? localSum : localSum - mat[n/2][n/2];
     }
 }
