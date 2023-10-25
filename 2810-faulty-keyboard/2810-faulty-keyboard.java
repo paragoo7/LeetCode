457f73e10dedbc9ce711fa08d5b6e75c6330a4ce
Parag Parate <123316083+paragoo7@users.noreply.github.com>
2023-10-25 11:45:29 +0530

    Time: 4 ms (32.57%), Space: 43.7 MB (64.52%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(c == 'i')    sb = sb.reverse();
+            else    sb.append(c+"");
+        }
+        return sb.toString();
+    }
+}
