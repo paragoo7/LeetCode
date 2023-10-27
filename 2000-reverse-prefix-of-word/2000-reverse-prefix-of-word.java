b7c1173f5e7e74294d99d36d9c0f1eaeea9f10f6
Parag Parate <123316083+paragoo7@users.noreply.github.com>
2023-10-27 12:47:10 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (28.55%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,11 +1,11 @@
 class Solution {
     public String reversePrefix(String word, char ch) 
     {
-        StringBuilder s = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         int index = word.indexOf(ch);
-        s.append(word.substring(0,index+1));
-        s.reverse();
-        s.append(word.substring(index+1));
-        return s.toString();
+        sb.append(word.substring(0,index+1));
+        sb.reverse();
+        sb.append(word.substring(index+1));
+        return sb.toString();
     }
 }
