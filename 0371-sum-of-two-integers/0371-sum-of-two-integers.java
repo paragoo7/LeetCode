1d96dba744a31f5e51b12641d526e188d431b854
Parag Parate <123316083+paragoo7@users.noreply.github.com>
2023-12-29 13:52:31 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (5.37%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int getSum(int a, int b) {       
+        if(b==0) return a;
+        if(b<0)
+            return getSum(--a,++b);
+        else
+            return getSum(++a,--b);
+    }
+}
